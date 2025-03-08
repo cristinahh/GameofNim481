88b7e7a9557685fe47a8078494003ff1a0728688
S-Fumoffu <160081528+S-Fumoffu@users.noreply.github.com>
2025-03-08 01:39:07 -0800

    WIP

--- game_of_nim.py
+++ game_of_nim.py
@@ -26,6 +26,9 @@ class GameOfNim(Game):
         return state.moves
     
     def result(self, state, move):
+        if move not in state.moves:
+            return state  # Illegal move has no effect
+        
         #create a copy of the board
         board = state.board.copy()
 
@@ -49,11 +52,18 @@ class GameOfNim(Game):
     
     def utility(self, state, player):
         """Return the value to player; 1 for win, -1 for loss, 0 otherwise."""
-        raise NotImplementedError
+        # return state.utility if player == 'MAX' else -state.utility
+        if not self.terminal_test(state):
+            return 0
+        elif player == 'MAX':
+            return 1
+        elif player == 'MIN':
+            return -1
     
     def terminal_test(self, state):
         """A state is terminal if there are no objects left"""
         return sum(state.board) == 0
+        # return state.utility != 0 or len(state.moves) == 0
     
     def display(self, state):
         board = state.board
@@ -65,9 +75,11 @@ if __name__ == "__main__":
     print(nim.initial.board) # must be [0, 5, 3, 1]
     print(nim.initial.moves) # must be [(1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (2,1), (2, 2), (2, 3), (3, 1)]
     print(nim.result(nim.initial, (1,3) ))
-    # utility = nim.play_game(alpha_beta_player, query_player) # computer moves first
+    utility = nim.play_game(alpha_beta_player, query_player) # computer moves first
 
-    # if (utility < 0):
-    #     print("MIN won the game")
-    # else:
-    #     print("MAX won the game")
+    if (utility < 0):
+        print("MIN won the game")
+    else:
+        print("MAX won the game")
+
+    print ("\n utility is ", utility)
