51b7f4e3f455015135c3f7600f7dd3a2673574da
S-Fumoffu <160081528+S-Fumoffu@users.noreply.github.com>
2025-03-07 23:36:39 -0800

    Slight edits

--- game_of_nim.py
+++ game_of_nim.py
@@ -11,6 +11,8 @@ class GameOfNim(Game):
         #first player is MAX
         #create a list of possible moves
 
+        self.board = board
+
         moves = []
         for r, row in enumerate(board):
             for n in range(1, row + 1):
@@ -24,7 +26,7 @@ class GameOfNim(Game):
         return state.moves
     
     def result(self, state, move):
-        #create a copu of the board
+        #create a copy of the board
         board = state.board.copy()
 
         #parse move
@@ -51,7 +53,7 @@ class GameOfNim(Game):
     
     def terminal_test(self, state):
         """A state is terminal if there are no objects left"""
-        raise sum(state.board) == 0
+        return sum(state.board) == 0
     
     def display(self, state):
         board = state.board
@@ -63,9 +65,9 @@ if __name__ == "__main__":
     print(nim.initial.board) # must be [0, 5, 3, 1]
     print(nim.initial.moves) # must be [(1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (2,1), (2, 2), (2, 3), (3, 1)]
     print(nim.result(nim.initial, (1,3) ))
-    utility = nim.play_game(alpha_beta_player, query_player) # computer moves first
+    # utility = nim.play_game(alpha_beta_player, query_player) # computer moves first
 
-    if (utility < 0):
-        print("MIN won the game")
-    else:
-        print("MAX won the game")
+    # if (utility < 0):
+    #     print("MIN won the game")
+    # else:
+    #     print("MAX won the game")
